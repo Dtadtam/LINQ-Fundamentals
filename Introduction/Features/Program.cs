8f56084d950262c15ba74180cfc9bc93a1ae9e36
CheawBakka <cyberutimate@gmail.com>
2017-05-31 00:27:05 +0700

    Add file via upload
    Starter Solution

---
+++ Introduction/Features/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name="Scott" },
+                new Employee { Id = 2, Name ="Chris" }
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            Console.WriteLine(developers.Count());
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    
+}
