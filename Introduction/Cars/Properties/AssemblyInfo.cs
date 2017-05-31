90835053bcd70666d480f0d3eb91b2a8c6c70aed
Supakit Inkaew <si@manaosoftware.com>
2017-05-31 18:17:02 +0700

    Update Code

---
+++ Introduction/Cars/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cars")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Manao Software")]
+[assembly: AssemblyProduct("Cars")]
+[assembly: AssemblyCopyright("Copyright © Manao Software 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8113a44b-67ff-45a8-8ef3-9f467d3d61a3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Introduction/Features/Program.cs
+++ Introduction/Features/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,16 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) => 
+            {
+                int temp = x + y;
+                return temp;
+            };
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(3, 5)));
+
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee { Id = 1, Name="Scott" },
@@ -21,15 +32,48 @@ namespace Features
                 new Employee { Id = 3, Name = "Alex" }
             };
 
-            Console.WriteLine(developers.Count());
-            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            //Console.WriteLine(developers.Count());
+            //IEnumerator<Employee> enumerator = developers.GetEnumerator();
 
-            while (enumerator.MoveNext())
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+
+            //foreach (var employee in developers.Where(NameStartsWiths))
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            //foreach (var employee in developers.Where(
+            //    delegate (Employee employee) 
+            //    {
+            //        return employee.Name.StartsWith("S");
+            //    }))
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            var query = developers.Where(e => e.Name.Length == 5)
+                                  .OrderByDescending(o => o.Name)
+                                  .Select(e => e);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name descending
+                         select developer;
+
+            foreach (var employee in query2)
             {
-                Console.WriteLine(enumerator.Current.Name);
+                Console.WriteLine(employee.Name);
             }
 
             Console.ReadLine();
+        }
+
+        private static bool NameStartsWiths(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
         }
     }
     
